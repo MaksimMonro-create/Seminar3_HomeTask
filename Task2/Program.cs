2df74c010153f79f8b106f01cd5fb79a20c86808
MaksimMonro-create <maksim.monro@yandex.ru>
2022-09-03 07:23:14 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// ВОЗВЕДИТЕ ЧИСЛО А В НАТУРАЛЬНУЮ СТЕПЕНЬ B, ИСПОЛЬЗУЯ ЦИКЛ
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение степени");
+int step = int.Parse(Console.ReadLine());
+int i;
+int result = number;
+for(i=2; i <=step; i++)
+{
+    result = result*number;
+}
+    if(step==1)
+    {
+        result = number;
+    }
+
+
+Console.WriteLine("Число в степени = " + result);
