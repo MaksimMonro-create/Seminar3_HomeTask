9e09b07ae64576edc0bd9bc9f2de60f8d803d53b
MaksimMonro-create <maksim.monro@yandex.ru>
2022-09-03 07:23:53 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// НАПИСАТЬ ПРОГРАММУ ВЫЧИСЛЕНИЯ ПРОИЗВЕДЕНИЯ ЧИСЕЛ ОТ 1 ДО N
+Console.Clear();
+Console.WriteLine();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int GetNumbersComposition(int x)
+{
+    int comp = 1;
+    for (int num = 1; num <= x; num++)
+    {
+        comp = comp * num;
+    }
+    return comp;
+}
+
+Console.WriteLine($"Произведение чисел от 1 до {N} равно {GetNumbersComposition(N)}");
+Console.WriteLine();
