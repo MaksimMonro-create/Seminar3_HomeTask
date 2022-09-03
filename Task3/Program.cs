d2979a67a7eb9d8270c9703c1b990f377ad600b1
MaksimMonro-create <maksim.monro@yandex.ru>
2022-09-03 07:23:35 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// НАЙДИТЕ СУММУ ЦИФР В ЧИСЛЕ
+
+int sum_of_numbers(int number)
+{
+    if(number < 10)
+    return number;
+    int num1 = number%10;
+    int next_number = number/10;
+    int sum = num1 + sum_of_numbers(next_number);
+    return sum;
+}
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {number} = {sum_of_numbers(number)}");
