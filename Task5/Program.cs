1ad5bd790efd2c578c1a25c01a42feca1ed13322
MaksimMonro-create <maksim.monro@yandex.ru>
2022-09-03 07:24:11 +0300

    Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// ПОКАЗАТЬ КУБЫ ЧИСЕЛ, ЗАКАНЧИВАЮЩИХСЯ НА ЧЕТНУЮ ЦИФРУ
+Console.Clear();
+Console.WriteLine();
+
+int N = new Random().Next(1, 30);
+Console.WriteLine($"N = {N}");
+int number = 1;
+while (number <= N)
+{
+    int cube = number * number * number;
+    if (cube%2 == 0) 
+    Console.Write(cube + " ");
+    number++;
+}
+Console.WriteLine();
